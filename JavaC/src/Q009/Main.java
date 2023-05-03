117ddf61d17817ec478d1ad7c13cd8cc53e1383c
sovisrushain <sovisrushain@gmail.com>
2023-05-03 17:02:22 +0530

    Q009

---
+++ JavaC/src/Q009/Main.java
@@ -0,0 +1,16 @@
+package Q009;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] listOne = {1, 2, 3};
+        int[] listTwo = {2, 1, 3};
+    }
+
+    public static boolean checkPermutation(int[] listOne, int[] listTwo) {
+        boolean isPermutation = listOne.length != listTwo.length;
+
+
+
+        return isPermutation;
+    }
+}
